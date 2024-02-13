e1d198269b7d5742ed223746621f1f831d1031fd
stefko992 <132852357+stefko992@users.noreply.github.com>
2024-02-13 10:54:20 +0200

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleApp3
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("bytove info");
+            Console.WriteLine($" short {sizeof(short)} , unshort {sizeof(ushort)} , int {sizeof(int)} , unint {sizeof(uint)} ," +
+                $" long {sizeof(long)} , unlong {sizeof(ulong)} , char {sizeof(char)} , float {sizeof(float)}," +
+                $" double {sizeof(double)}, decimal {sizeof(decimal)}, byte {sizeof(byte)} , shortbyte {sizeof(sbyte)}");
+
         }
     }
 }
